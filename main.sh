3d43e3560ce2e0420f013998659d8996da2f9942
Rodolfo De Nadai <rdenadai@ccuec.unicamp.br>
2017-08-07 16:43:05 -0300

    - Improvements

--- main.sh
+++ main.sh
@@ -8,6 +8,8 @@ kill $(ps aux | grep '.pyenv' | awk '{print $2}')
 
 python /usr/src/app/main.py
 
+hass --open-ui &
+
 service nginx restart
 
 jupyter notebook --ip=0.0.0.0 --port=8585 --no-browser -y --allow-root &
