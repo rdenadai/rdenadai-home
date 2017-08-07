fd4a1351b91be20b6d03c04bfe1e8346c0234b55
Rodolfo De Nadai <rdenadai@ccuec.unicamp.br>
2017-08-07 13:57:53 -0300

    - Improvements

--- main.sh
+++ main.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+hass
+
+sleep 15
+
+kill $(ps aux | grep '.pyenv' | awk '{print $2}')
 
 python /usr/src/app/main.py
 
-service nginx start &
+service nginx start
 
-hass --open-ui &
-
-jupyter notebook --ip=0.0.0.0 --port=8585 --no-browser -y &
+jupyter notebook --ip=0.0.0.0 --port=8585 --no-browser -y --allow-root &
 
 /root/duckdns/duck.sh &
