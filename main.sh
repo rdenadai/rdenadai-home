8e64aa09e16fdb83d3ba89200be92753e47ff29d
Rodolfo De Nadai <rdenadai@ccuec.unicamp.br>
2017-08-08 12:56:54 -0300

    - Some minor tweaks in main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+echo "Modprobe i2c-dev."
+
 modprobe i2c-dev
+
+echo "Modprobe bcm2835-v4l2."
 
 modprobe bcm2835-v4l2
 
+echo "Python main.py environment vars."
+
 python /usr/src/app/main.py
+
+echo "Starting Home Assistant."
 
 hass --open-ui &
 
-service nginx restart
+echo "Starting jupyter notebooks."
 
 jupyter notebook --ip=localhost --port=8585 --no-browser -y --allow-root &
+
+echo "Restarting nginx."
+
+service nginx restart
+
+echo "Duckdns magic!"
 
 /root/duckdns/duck.sh &
